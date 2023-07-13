3f10abea17ff715b00bb6eaaa73367919c7280b3
Sa-ndy763 <111154266+Sa-ndy763@users.noreply.github.com>
2023-07-12 20:52:03 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADDGroup4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult sum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult add()
+        {
+            int num1 = Convert.ToInt32(HttpContext.Request.Form["txtFirst"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["txtSecond"].ToString());
+            int result = num1 + num2;
+            ViewBag.SumResult = result.ToString();
+            return View("sum");
+        }
+    }
+}
